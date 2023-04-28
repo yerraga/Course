0956676f0d1a60fd98f548ec9c57c31174ea9de6
Ganesh <ganeshyadv1997@gmail.com>
2023-04-28 20:21:43 +0530

    Course application has been created

---
+++ demo/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into course (id, name) values (1001, 'Jpa Repository in 50 steps');
+insert into course (id, name) values (1002, 'SpringBoot in 50 steps');
+insert into course (id, name) values (1003, 'Java in 50 steps');
+insert into course (id, name) values (1004, 'Microservices in 50 steps');
+
+# Student details
+insert into Student (id, name) values (2001, 'Ganesh');
+insert into Student (id, name) values (2001, 'Sunil');
+insert into Student (id, name) values (2001, 'Anasuyamma');
+insert into Student (id, name) values (2001, 'Narashimha Reddy');
